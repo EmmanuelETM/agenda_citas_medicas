a168d8eb3b94a5284eefb5143160ba9d78489c37
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-03-14 20:09:04 -0400

    Create branch portal_admin and test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script>
     import "../app.css";
+
+    let count = 0;
+
+    function increment(){
+        count++;
+    }
 </script>
 
 
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<button on:click={increment}>
+    clicked {count}
+    {count === 1 ? "time" : "times"}
+</button>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Very poggers indeed</h1>
