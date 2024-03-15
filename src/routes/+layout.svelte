12d37f5274241337901a70c34070a6879c8f45b0
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-03-15 16:31:31 -0400

    Remove tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
-<nav>
+<nav class = "">
 	<a href="/">home</a>
     <a href="/about">about</a>
+    <a href="/portal_admin">admin</a>
 </nav>
 
 <slot />
---
+++ src/routes/portal_admin/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../../app.css";
+    import Menu from './components/menu.svelte';
+
+</script>
+
+<main>    
+
+</main>
+
+
+
+ 
