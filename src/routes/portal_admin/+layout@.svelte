8c73c974fa2d4b437a3cc0d396dda718b01e365c
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-03-24 14:09:26 -0400

    Create branch chat

--- src/routes/portal_admin/+layout@.svelte
+++ src/routes/portal_admin/+layout@.svelte
@@ -6,16 +6,8 @@
 </script>
 
 <div class="index min-h-screen">
-    <!-- <Nav/> -->
     <Sidebar/>
     <slot/>
 </div>
 <Footer/>
 
-<!-- <style>
-    .index{
-        display: grid;
-        grid-template-columns: repeat(12, minmax(0, 1fr));
-        grid-auto-rows: minmax(75px, auto);
-    }
-</style> -->
